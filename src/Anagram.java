79d414da5385266085115177fc915318e2baa23d
11002288 <league@WTS5.attlocal.net>
2018-07-09 17:00:32 -0700

    alphabetize the array not the scanner
    asdf

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,32 +1,50 @@
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Anagram {
 
 	public static void main(String[] args) {
+
 		try {
 			Scanner scan = new Scanner(new FileReader("src/words.txt"));
 
 			ArrayList<String> array = new ArrayList<String>();
-			
-			int num = 0;
 
+			String word = JOptionPane.showInputDialog("Type in a word");
+			scan.nextLine().toLowerCase();
+			
+			String sc = scan.toString();
+			char[] noMom = sc.toCharArray();
+			Arrays.sort(noMom);
+			String more = new String(noMom);
+			System.out.println(more);
+			
+			char[] chars = word.toCharArray();
+			Arrays.sort(chars);
+			String mod = new String(chars);
+			System.out.println(mod);
+			
 			while (scan.hasNextLine() == true) {
-				String line = scan.nextLine();
-				
-				array.add(scan.nextLine());
-				if (array.get(num).equals(line)) {
-					System.out.println("test");
-				}
-				
+				if (scan.nextLine().length() == (mod.length())) {
+					
+					array.add(scan.nextLine());
 
-		
-			
-				
+				}
+
 			}
+
+			System.out.println(array);
+
+			// if (array) {
+			// System.out.println("test");
+			// }
 
 		} catch (Exception e) {
 
