b869f70c720188c17fc16bea035bdbe9818806f8
11002288 <league@wts20.attlocal.net>
2018-07-22 11:33:22 -0700

    idk

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,25 +1,30 @@
 package _01_array_manipulation;
 
-import java.util.ArrayList;
-
 public class Inserting {
 
-	public static int[] insertAt(int[] testArray, int i, int j) {
+	public static int[] insertAt(int[] oldArray, int indexToInsertInto, int valueToInsert) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> num = new ArrayList<Integer>();
-		for (int of : testArray) {
-			num.add(of);
-			num.add(j, 0);
-			num.add(j, 4);
-			num.add(j, 5);
-		}
+
+		int[] newArray = new int[oldArray.length + 1];
+
+		for (int k = 0; k < newArray.length; k++) {
+			
+			if (k==indexToInsertInto) {
+				newArray[k]=valueToInsert;
+			}else {
+				newArray[k]=oldArray[k-1];
+				System.out.println(oldArray[k]);
 				
-		return num;
+			}
+			
+		}
+
+		return newArray;
 	}
 
 	public static String[] insertAlphabetically(String[] orderedArray, String string) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
