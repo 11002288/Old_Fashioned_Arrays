3f2e7fc17d4335bf0eb68621ddac4f3f7c143928
League <League@AdminisorsiMac2.attlocal.net>
2018-07-23 16:57:24 -0700

    inserting
    check failed test and comment them out

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,5 +1,7 @@
 package _01_array_manipulation;
 
+import java.util.Arrays;
+
 public class Inserting {
 
 	public static int[] insertAt(int[] oldArray, int indexToInsertInto, int valueToInsert) {
@@ -7,24 +9,68 @@ public class Inserting {
 
 		int[] newArray = new int[oldArray.length + 1];
 
-		for (int k = 0; k < newArray.length; k++) {
-			
-			if (k==indexToInsertInto) {
-				newArray[k]=valueToInsert;
-			}else {
-				newArray[k]=oldArray[k-1];
-				System.out.println(oldArray[k]);
-				
+		for (int k = 0; k < oldArray.length; k++) {
+
+			if (k == indexToInsertInto) {
+				newArray[indexToInsertInto] = valueToInsert;
+
+			} else {
+				newArray[k] = oldArray[k];
+
 			}
-			
+
 		}
 
 		return newArray;
+
 	}
 
-	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+	public static void sortArray(String[] old) {
+	
+	String temp;
+	for (int i = 0; i < old.length-1; i++) {
+        for (int j = 1; j < (old.length-1 - i); j++) {
+
+            if (old[j - 1] .compareTo(old[j])>0) {
+                temp = old[j - 1];
+                old[j - 1] = old[j];
+                old[j] = temp;
+            }
+
+        }
+    }
+	
+		
+		
+		
+	}
+	public static String[] insertAlphabetically(String[] orderedArray, String value) {
 		// TODO Auto-generated method stub
-		return null;
+		sortArray(orderedArray);
+		String[] list = new String[orderedArray.length + 1];
+		
+		boolean going = false;
+		int index = 0;
+		
+		for (String a : orderedArray) {
+			if (a.compareTo(value)<=0) {
+				list[index] = a;
+				index += 1;
+			
+			}else {
+				list[index] = value;
+				index += 1;
+				list[index] = a;
+				index += 1;
+			}
+			
+		}	
+		for (String string : list) {
+			System.out.println(string);
+		}				
+		
+
+		return list;
 	}
 
 }
