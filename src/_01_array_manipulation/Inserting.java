31c70d5f5c7a15e12bd512615581eb60fa14c9ed
11002288 <league@WTS22.attlocal.net>
2018-07-25 16:53:24 -0700

    done start 0_2_diy_array_list
    done

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,23 +1,26 @@
 package _01_array_manipulation;
 
-import java.util.Arrays;
-
 public class Inserting {
 
 	public static int[] insertAt(int[] oldArray, int indexToInsertInto, int valueToInsert) {
 		// TODO Auto-generated method stub
-
+		int number = 0;
 		int[] newArray = new int[oldArray.length + 1];
 
 		for (int k = 0; k < oldArray.length; k++) {
 
-			if (k == indexToInsertInto) {
+			if (number == indexToInsertInto) {
 				newArray[indexToInsertInto] = valueToInsert;
-
+				// number += 1;
+				number = k + 1;
+				newArray[number] = oldArray[k];
 			} else {
-				newArray[k] = oldArray[k];
 
+				newArray[number] = oldArray[k];
+				// number = +1;
 			}
+
+			number++;
 
 		}
 
@@ -26,49 +29,55 @@ public class Inserting {
 	}
 
 	public static void sortArray(String[] old) {
-	
-	String temp;
-	for (int i = 0; i < old.length-1; i++) {
-        for (int j = 1; j < (old.length-1 - i); j++) {
 
-            if (old[j - 1] .compareTo(old[j])>0) {
-                temp = old[j - 1];
-                old[j - 1] = old[j];
-                old[j] = temp;
-            }
+		String temp;
+		for (int i = 0; i < old.length; i++) {
+			for (int j = 1; j < (old.length); j++) {
 
-        }
-    }
-	
-		
-		
-		
+				if (old[j - 1].compareTo(old[j]) > 0) {
+					temp = old[j - 1];
+					old[j - 1] = old[j];
+					old[j] = temp;
+				}
+
+			}
+		}
+		// for (String it : old) {
+		// System.out.println(it);
+		// }
 	}
+
 	public static String[] insertAlphabetically(String[] orderedArray, String value) {
 		// TODO Auto-generated method stub
 		sortArray(orderedArray);
 		String[] list = new String[orderedArray.length + 1];
-		
+
 		boolean going = false;
 		int index = 0;
-		
+
 		for (String a : orderedArray) {
-			if (a.compareTo(value)<=0) {
+			if (a.compareTo(value) <= 0 || going) {
 				list[index] = a;
 				index += 1;
-			
-			}else {
+
+			} else {
 				list[index] = value;
+				going = true;
 				index += 1;
+
 				list[index] = a;
 				index += 1;
+
 			}
-			
-		}	
+
+		}
+		if (!going) {
+			list[index] = value;
+		}
+
 		for (String string : list) {
 			System.out.println(string);
-		}				
-		
+		}
 
 		return list;
 	}
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -1,33 +1,34 @@
 package _01_array_manipulation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Sorting {
 
-	public static void sort(String[] testArray) {
+	public static void sort(String[] old) {
 		// TODO Auto-generated method stub
-			
-		
-		
-	/*	String ta = "";
-		ArrayList<String> list = new ArrayList<String>();
-		ArrayList<String> list2 = new ArrayList<String>();
-		ta.equals(testArray);
-		char[] chars = ta.toCharArray();
-		Arrays.sort(chars);
-		String change = new String(chars);
 
-		for (int i = 0; i < list.size(); i++) {
-			list.add(change);
-			if (list.get(i).equals(list.get(i))) {
-System.out.println(list);
+		String temp;
+		for (int i = 0; i < old.length; i++) {
+			for (int j = 1; j < (old.length); j++) {
+
+				if (old[j - 1].compareTo(old[j]) > 0) {
+					temp = old[j - 1];
+					old[j - 1] = old[j];
+					old[j] = temp;
+				}
+
 			}
-
 		}
-*/
+
 	}
 
-	
-
+	/*
+	 * String ta = ""; ArrayList<String> list = new ArrayList<String>();
+	 * ArrayList<String> list2 = new ArrayList<String>(); ta.equals(testArray);
+	 * char[] chars = ta.toCharArray(); Arrays.sort(chars); String change = new
+	 * String(chars);
+	 * 
+	 * for (int i = 0; i < list.size(); i++) { list.add(change); if
+	 * (list.get(i).equals(list.get(i))) { System.out.println(list); }
+	 * 
+	 * }
+	 */
 }
